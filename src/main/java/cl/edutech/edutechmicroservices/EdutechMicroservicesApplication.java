67b58b1b6cdf1a83461bd5958f9ecc1190d52582
Dario <dar.moralesa@duocuc.cl>
2025-04-24 23:08:42 -0400

    Primer commit

---
+++ src/main/java/cl/edutech/edutechmicroservices/EdutechMicroservicesApplication.java
@@ -0,0 +1,13 @@
+package cl.edutech.edutechmicroservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EdutechMicroservicesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EdutechMicroservicesApplication.class, args);
+    }
+
+}
