dcc9f52884a6a22166b581d3d5387e891d5049e9
dariomorales1 <dar.moralesa@duocuc.cl>
2025-04-27 15:45:47 -0400

    Creacion del modulo courseservice

---
+++ courseservise/src/main/java/cl/edutech/courseservise/CourseserviseApplication.java
@@ -0,0 +1,13 @@
+package cl.edutech.courseservise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourseserviseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CourseserviseApplication.class, args);
+    }
+
+}
---
+++ courseservise/src/main/java/cl/edutech/courseservise/domain/Course.java
@@ -0,0 +1,13 @@
+package cl.edutech.courseservise.domain;
+
+import jakarta.persistence.Entity;
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Builder
+public class Course {
+}
